d011b869e3e29a1f234d57ce12d6ef24b86f36d1
Konstantin <parnishkafin@gmail.com>
2023-04-11 20:33:25 +0300

    Nachal test

---
+++ src/test/java/AviaSoulsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class AviaSoulsTest {
+    Ticket ticket1 = new Ticket("Казань", "Уфа", 4000, 10, 11);
+    Ticket ticket2 = new Ticket("Казань", "Москва", 8000, 10, 13);
+    Ticket ticket3 = new Ticket("Казань", "Самара", 3000, 10, 14);
+    Ticket ticket4 = new Ticket("Казань", "Воронеж", 6000, 10, 15);
+    Ticket ticket5 = new Ticket("Казань", "Уфа", 5000, 10, 12);
+
+
+    @Test
+
+
+
+}
