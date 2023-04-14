faeac923694898daa5b2bc4294065cd93c9a096f
Konstantin <parnishkafin@gmail.com>
2023-04-14 20:24:59 +0300

    bez testov

--- src/main/java/TicketTimeComparator.java
+++ src/main/java/TicketTimeComparator.java
@@ -6,7 +6,18 @@ public class TicketTimeComparator implements Comparator<Ticket> {
     public int compare(Ticket t1, Ticket t2) {
         // ваш код
 
-        
+
+        if (t1.getTimeTo() - t1.getTimeFrom() < t2.getTimeTo() - t2.getTimeFrom()) {
+            return -1;
+        } else if (t1.getTimeTo() - t1.getTimeFrom() > t2.getTimeTo() - t2.getTimeFrom()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
+
 }
+
+
+
 
